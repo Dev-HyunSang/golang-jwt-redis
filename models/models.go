a252d49f7cd76bec943b6d1b5cea04558fc226e5
Dev-HyunSang <me@hyunsang.dev>
2023-01-24 21:39:56 +0900

    feat: JWT Token 관련 구조체 추가함.

--- models/models.go
+++ models/models.go
@@ -16,6 +16,17 @@ type User struct {
 	UpdatedAt    time.Time `json:"updated_at"`
 }
 
+type TokenDetails struct {
+	UserUUID     uuid.UUID `json:"user_uuid"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+	AccessUUID   uuid.UUID `json:"access_uuid"`
+	RefreshUUID  uuid.UUID `json:"refresh_uuid"`
+	AtExpires    int64     `json:"at_expires"`
+	RtExpires    int64     `json:"rt_expires"`
+}
+
+// === Request ===
 type RequestJoinUser struct {
 	Email    string `json:"email"`
 	Password string `json:"password"`
@@ -23,14 +34,21 @@ type RequestJoinUser struct {
 }
 
 type RequestLoginUser struct {
-	Email string `json:"email"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
+// === Response ===
 type MetaData struct {
 	Status     string `json:"status"`
 	StatusCode int    `json:"status_code"`
 	Success    bool   `json:"success"`
 	Message    string `json:"message"`
+}
+
+type ResponseError struct {
+	Meta        MetaData  `json:"meta"`
+	ResponsedAt time.Time `json:"responsed_at"`
 }
 
 type ResponseOkJoinUser struct {
@@ -39,7 +57,14 @@ type ResponseOkJoinUser struct {
 	ResponsedAt time.Time `json:"responsed_at"`
 }
 
-type ResponseError struct {
-	Meta        MetaData  `json:"meta"`
-	ResponsedAt time.Time `json:"responsed_at"`
+type TokenDatas struct {
+	UserUUID     uuid.UUID `json:"user_uuid"`
+	AccessToken  string    `json:"access_token"`
+	RefreshToken string    `json:"refresh_token"`
+}
+
+type ResponseOkLoginUser struct {
+	Meta        MetaData   `json:"meta"`
+	Data        TokenDatas `json:"data"`
+	ResponsedAt time.Time  `json:"responsed_at"`
 }
