3dd6b028d0389140574fc5a68706ee3e338c8f7d
Dev-HyunSang <me@hyunsang.dev>
2023-01-31 17:51:21 +0900

    feat: 토큰 검증 시 토큰 없는 경우 오류 반환함.

--- auth/verify.go
+++ auth/verify.go
@@ -1,1 +1,49 @@
 package auth
+
+import (
+	"errors"
+	"fmt"
+	"github.com/dev-hyunsang/golang-jwt-redis/config"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"strings"
+)
+
+func ExtractToken(r *fiber.Ctx) (string, error) {
+	bearToken := r.GetReqHeaders()
+
+	jwtString := strings.Split(bearToken["Authorization"], "Bearer ")[1]
+	if len(jwtString) == 0 { // 만약 토큰 없는 경우 오류 반환함.
+		return "", errors.New("Failed to Reading JSON Web Token.")
+	}
+
+	return jwtString, nil
+}
+
+func VerifyToken(r *fiber.Ctx) (*jwt.Token, error) {
+	tokenString, err := ExtractToken(r)
+	if err != nil {
+		return nil, err
+	}
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(config.GetEnv("ACCESS_SECRET")), nil
+	})
+
+	return token, err
+}
+
+func TokenVailed(r *fiber.Ctx) error {
+	token, err := VerifyToken(r)
+	if err != nil {
+		return err
+	}
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return err
+	}
+
+	return nil
+}
